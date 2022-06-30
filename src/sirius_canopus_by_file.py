3f38f20efe9dd61cc1fd1874a9ae31e9b0955604
Arnaud Gaudry <gaudry.arnaud@gmail.com>
2022-06-30 18:10:08 +0200

    typos

--- src/sirius_canopus_by_file.py
+++ src/sirius_canopus_by_file.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pandas as pd
 import subprocess
 import shutil
+from tqdm import tqdm
 
 my_env = os.environ.copy()
 # my_env["GUROBI_HOME"] = "/prog/gurobi951/linux64/"
@@ -18,7 +19,7 @@ with open (r'configs/user/user.yml') as file:
 path_to_data = params_list['paths'][0]['path_to_data']
 
 sirius_version = params_list['options'][0]['sirius_version']
-ioniziation = params_list['options'][1]['ioniziation']
+ionization = params_list['options'][1]['ionization']
 sirius_command = params_list['options'][2]['sirius_command']
 recompute = params_list['options'][3]['recompute']
 zip_output = params_list['options'][4]['zip_output']
@@ -31,25 +32,25 @@ def compute_sirius_canopus(file, output_name):
                       
 path = os.path.normpath(path_to_data)
 samples_dir = [directory for directory in os.listdir(path)]
-
-for directory in samples_dir:
+for directory in tqdm(samples_dir):
     metadata_path = os.path.join(path, directory, directory + '_metadata.tsv')
     try:
         metadata = pd.read_csv(metadata_path, sep='\t')
     except FileNotFoundError:
         continue
     except NotADirectoryError:
-        continue
+        continue 
+    
     # Check if the sample type is sample and not QC or Blank
-    if metadata['sample_type'][0] == 'sample':
-        if ioniziation == 'pos':    
-            sirius_mgf_path = os.path.join(path, directory,ioniziation, directory + '_sirius_pos.mgf')
-        elif ioniziation == 'neg':    
-            sirius_mgf_path = os.path.join(path, directory,ioniziation, directory + '_sirius_neg.mgf')
+    if metadata['sample_type'][0] == 'sample':   
+        if ionization == 'pos': 
+            sirius_mgf_path = os.path.join(path, directory,ionization, directory + '_sirius_pos.mgf')
+        elif ionization == 'neg':    
+            sirius_mgf_path = os.path.join(path, directory,ionization, directory + '_sirius_neg.mgf')
         else:
-            raise ValueError("ioniziation parameter must be pos or neg")   
-           
-        output_folder = os.path.join(path, directory, ioniziation, directory + '_' + output_suffix)        
+            raise ValueError("ionization parameter must be pos or neg")   
+        
+        output_folder = os.path.join(path, directory, ionization, directory + '_' + output_suffix)        
         if (recompute is False) & (os.path.isdir(output_folder)):
             print(f"Skipped already computed sample: {directory}")          
             continue
@@ -77,7 +78,7 @@ for directory in samples_dir:
                         shutil.make_archive(os.path.join(output_folder, dir), 'zip', os.path.join(output_folder, dir))
                         shutil.rmtree(os.path.join(output_folder, dir))
                           
-            shutil.copyfile(r'configs/user/user.yml', os.path.join(path, directory, ioniziation, directory + '_' + output_suffix, 'params.yml'))
+            shutil.copyfile(r'configs/user/user.yml', os.path.join(path, directory, ionization, directory + '_' + output_suffix, 'params.yml'))
             
             print(f"Sample: {directory} done")
                 
